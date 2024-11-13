3124896c0ab56313c2ec854d2069d7232e7417a0
Marcelo Zaniolo <imarcelolz@gmail.com>
2024-11-13 22:29:07 +0100

    Briken main

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,96 @@
-#include "wifi.h"
-// #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <string>
+#include <HardwareSerial.h>
+#include "BluetoothSerial.h"
 
+const String DEVICE_NAME = "ESP-Monitor";
+const int BRIGHTNESS = 2;
+const int LDC_HEIGHT = 2;
+const int LDC_WITDH = 16;
+const int LOOP_DELAY = 500;
 const int SERIAL_BAUD = 9600;
-const String ssid = "";
-const String password = "";
 
 struct Data {
   String firstLine;
   String secondLine;
 };
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-void print(String firstLine, String secondLine);
-Data fetchData();
+void connectBT();
+void setPaitingMessage();
+Data* readData();
+void update(Data* data);
+
+LiquidCrystal_I2C lcd(0x27, LDC_WITDH, LDC_HEIGHT);
+BluetoothSerial serialBt;
+
+Data data = {
+  "",
+  ""
+};
+
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
+
   lcd.init();
   lcd.backlight();
 
-  print("Wifi", "Connecting...");
-  wifiConnect(ssid, password);
+  connectBT();
+  // lcd.clear();
+
+  // lcd.setCursor(0, 0);
+  // lcd.print("Abacateiro");
+  // lcd.setCursor(0, 1);
+  // lcd.print("Juvelino");
 }
 
 void loop() {
-  Data data = fetchData();
 
-  print(data.firstLine, data.secondLine);
+  // Serial.print(".");
+  // Serial.print(data.firstLine + " ");
+  // Serial.println(data.secondLine);
 
-  delay(2250);
+  // if (serialBt.available()) {
+  //   update(readData());
+  // }
+
+  // if (!serialBt.hasClient()) {
+  //   connectBT();
+  // }
+
+  delay(LOOP_DELAY);
 }
 
-void print(String firstLine, String secondLine = "") {
+void setPaitingMessage() {
+  data.firstLine = "BT pair with";
+  data.secondLine = DEVICE_NAME;
+
+  update(&data);
+}
+
+void connectBT() {
+  Serial.println("Connecting to Bluetooth");
+
+  serialBt.begin(DEVICE_NAME);
+  setPaitingMessage();
+}
+
+Data* readData() {
+  Serial.println("Reading data");
+
+  data.firstLine = serialBt.readStringUntil(';');
+  data.secondLine = serialBt.readStringUntil('\n');
+
+  return &data;
+}
+
+void update(Data* data) {
   lcd.clear();
+
   lcd.setCursor(0, 0);
-  lcd.print(firstLine);
+  lcd.print(data->firstLine);
   lcd.setCursor(0, 1);
-  lcd.print(secondLine);
+  lcd.print(data->secondLine);
+
+  // TODO: Enable/disable pins
 }
-
-
-Data fetchData() {
-  return {
-    "Hello, World!",
-    "This is a test"
-  };
-}
-
